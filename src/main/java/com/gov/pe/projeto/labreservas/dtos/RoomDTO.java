95f9314847238963d77c7285b63c9f81c7faf381
Unknow <Unknow>
2024-09-08 23:57:37 -0300

    criando o DTO(Data Transfer Object) para praticar o conhecimento sobre a tecnica utilizada

---
+++ src/main/java/com/gov/pe/projeto/labreservas/dtos/RoomDTO.java
@@ -0,0 +1,15 @@
+package com.gov.pe.projeto.labreservas.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomDTO {
+    private Long id;
+    private String name;
+}
