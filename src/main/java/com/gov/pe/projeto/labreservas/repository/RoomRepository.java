25c1ad88ce060d7f57b9c5db6902bb45943760b1
Unknow <Unknow>
2024-09-08 23:58:03 -0300

    movendo o repositorio pra dentro de seu pacote

---
+++ src/main/java/com/gov/pe/projeto/labreservas/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.gov.pe.projeto.labreservas.repository;
+
+import com.gov.pe.projeto.labreservas.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+}
