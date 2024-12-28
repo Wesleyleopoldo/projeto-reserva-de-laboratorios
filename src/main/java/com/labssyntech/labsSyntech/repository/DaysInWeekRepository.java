5ee469b0ba51b2c04d602a45323461007f3f48b5
Wesley Silva <wesleycauan01@gmail.com>
2024-12-28 17:09:30 -0300

    Adiciona consulta personalizada...
    Signed-off-by: Wesley Silva <wesleycauan01@gmail.com>

--- src/main/java/com/labssyntech/labsSyntech/repository/DaysInWeekRepository.java
+++ src/main/java/com/labssyntech/labsSyntech/repository/DaysInWeekRepository.java
@@ -1,5 +1,9 @@
 package com.labssyntech.labsSyntech.repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +11,7 @@ import com.labssyntech.labsSyntech.models.DaysInTheWeek;
 
 @Repository
 public interface DaysInWeekRepository extends JpaRepository<DaysInTheWeek, Long>{
+
+    Optional<List<DaysInTheWeek>> findByOrganization_OrganizationId(UUID uuid);
+
 }
