966d9113cc420b25e05f143a03cd8cacbd9671a6
Wesley Silva <wesleycauan01@gmail.com>
2024-12-28 17:02:36 -0300

    Adapta Body para somente receber a lista de enums
    Signed-off-by: Wesley Silva <wesleycauan01@gmail.com>

--- src/main/java/com/labssyntech/labsSyntech/requestbodys/DaysInWeekRequestBody.java
+++ src/main/java/com/labssyntech/labsSyntech/requestbodys/DaysInWeekRequestBody.java
@@ -1,9 +1,8 @@
 package com.labssyntech.labsSyntech.requestbodys;
 import java.util.List;
-import java.util.UUID;
 
 import com.labssyntech.labsSyntech.utils.DaysInTheWeekEnum;
 
-public record DaysInWeekRequestBody(List<DaysInTheWeekEnum> daysInTheWeekEnums, UUID organizationId) {
+public record DaysInWeekRequestBody(List<DaysInTheWeekEnum> daysInTheWeekEnums) {
     
 }
