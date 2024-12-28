29a363c70f80778c32d24ef57e34963d4a613f40
Wesley Silva <wesleycauan01@gmail.com>
2024-12-28 17:03:47 -0300

    Adiciona tratamento de exceções e colocando o uuid da empresa nos parametros
    Signed-off-by: Wesley Silva <wesleycauan01@gmail.com>

--- src/main/java/com/labssyntech/labsSyntech/controllers/DaysInWeekController.java
+++ src/main/java/com/labssyntech/labsSyntech/controllers/DaysInWeekController.java
@@ -1,28 +1,47 @@
 package com.labssyntech.labsSyntech.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.labssyntech.labsSyntech.dtos.DaysInWeekDTO;
+import com.labssyntech.labsSyntech.dtos.DaysInWeekDTOSet;
 import com.labssyntech.labsSyntech.requestbodys.DaysInWeekRequestBody;
 import com.labssyntech.labsSyntech.services.DaysInWeekService;
 
 @Controller
-@RequestMapping("/register")
+@RequestMapping("/daysinweek")
 public class DaysInWeekController {
     
     @Autowired
     private DaysInWeekService daysInWeekService;
 
-    @PostMapping
-    public ResponseEntity<List<DaysInWeekDTO>> createDaysInTheWeek(@RequestBody DaysInWeekRequestBody data) {
-        List<DaysInWeekDTO> daysInWeekDTOs = daysInWeekService.createDaysInWeek(data.daysInTheWeekEnums(), data.organizationId());
-        return ResponseEntity.ok(daysInWeekDTOs);
+    @PostMapping("/register/{id}")
+    public ResponseEntity<?> createDaysInTheWeek(@PathVariable("id") UUID organizationId, @RequestBody DaysInWeekRequestBody data) {
+        
+        try {
+            List<DaysInWeekDTO> daysInWeekDTOs = daysInWeekService.createDaysInWeek(data.daysInTheWeekEnums(), organizationId);
+            return ResponseEntity.ok(daysInWeekDTOs);
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(409).body(e.getMessage());
+        }
+            
+    }
+
+    // Lista todas organizações...
+    @GetMapping("/days")
+    public ResponseEntity<List<DaysInWeekDTOSet>> getAllDaysInWeek() {
+
+        List<DaysInWeekDTOSet> daysInWeekDTOSets = daysInWeekService.getAllInWeekServices();
+
+        return ResponseEntity.ok(daysInWeekDTOSets);
     }
 }
