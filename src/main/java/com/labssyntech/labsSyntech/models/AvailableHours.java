a5191d760fcb07ce6647ed8d3e699590ae56bba3
Wesley Silva <wesleycauan01@gmail.com>
2025-02-24 09:09:45 -0300

    Adiciona nova coluna a entidade de horas disponíveis

--- src/main/java/com/labssyntech/labsSyntech/models/AvailableHours.java
+++ src/main/java/com/labssyntech/labsSyntech/models/AvailableHours.java
@@ -17,7 +17,7 @@ public class AvailableHours {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "available_hours_id", nullable = false)
-    private Long availableHours;
+    private Long availableHoursId;
 
     @Column(name = "hours", nullable = false)
     private LocalTime hours;
@@ -26,19 +26,22 @@ public class AvailableHours {
     @JoinColumn(name = "fk_organization_id", referencedColumnName = "organization_id", nullable = false)
     private Organization organization;
 
-    
-
     @ManyToOne
     @JoinColumn(name = "fk_days_in_the_week_id", referencedColumnName = "days_in_the_week_id", nullable = false)
     private DaysInTheWeek daysInTheWeek;
 
+    @ManyToMany(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "fk_room_id", referencedColumnName = "room_id", nullable = false)
+    private Room roomId;
+
     public AvailableHours() {
     }
 
-    public AvailableHours(Long availableHours, LocalTime hours, DaysInTheWeek daysInTheWeek) {
-        this.availableHours = availableHours;
+    public AvailableHours(Long availableHours, LocalTime hours, DaysInTheWeek daysInTheWeek, Room roomId) {
+        this.availableHoursId = availableHours;
         this.hours = hours;
         this.daysInTheWeek = daysInTheWeek;
+        this.roomId = roomId;
     }
 
     public DaysInTheWeek getDaysInTheWeek() {
@@ -57,12 +60,12 @@ public class AvailableHours {
         this.hours = hours;
     }
     
-    public Long getAvailableHours() {
-        return availableHours;
+    public Long getAvailableHoursId() {
+        return availableHoursId;
     }
 
-    public void setAvailableHours(Long availableHours) {
-        this.availableHours = availableHours;
+    public void setAvailableHoursId(Long availableHours) {
+        this.availableHoursId = availableHours;
     }
     
     public Organization getOrganization() {
@@ -72,4 +75,13 @@ public class AvailableHours {
     public void setOrganization(Organization organization) {
         this.organization = organization;
     }
+
+    public Room getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Room roomId) {
+        this.roomId = roomId;
+    }
+    
 }
