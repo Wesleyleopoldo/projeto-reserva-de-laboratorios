9c16aa518bf81f2daa1a528a75f834d24e371e2f
Wesley Silva <wesleycauan01@gmail.com>
2025-02-26 09:09:56 -0300

    Adiciona novos atributos ao responseDTO de usuário

--- src/main/java/com/labssyntech/labsSyntech/dto/UserDTO.java
+++ src/main/java/com/labssyntech/labsSyntech/dto/UserDTO.java
@@ -2,14 +2,10 @@ package com.labssyntech.labsSyntech.dto;
 
 import java.util.UUID;
 
-import com.labssyntech.labsSyntech.models.Organization;
-
 public record UserDTO(
     UUID userId, 
     String userName, 
     String email,
-    String password,
-    boolean isPresident,
-    Organization organization
+    boolean isPresident
 ) {
 }
