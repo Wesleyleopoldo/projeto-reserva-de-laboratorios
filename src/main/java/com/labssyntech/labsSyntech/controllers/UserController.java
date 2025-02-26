cf68481cc1613496f3a2a1ed34c8cff1f58535ce
Wesley Silva <wesleycauan01@gmail.com>
2025-02-26 09:09:04 -0300

    Adiciona novos endpoints para atualização de dados do usuário

--- src/main/java/com/labssyntech/labsSyntech/controllers/UserController.java
+++ src/main/java/com/labssyntech/labsSyntech/controllers/UserController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.labssyntech.labsSyntech.dto.UserDTO;
 import com.labssyntech.labsSyntech.requests.PresidentSignupRequest;
+import com.labssyntech.labsSyntech.requests.UserRequestBody;
 import com.labssyntech.labsSyntech.requests.UserRequestUuid;
 import com.labssyntech.labsSyntech.requests.UserSignupRequest;
 import com.labssyntech.labsSyntech.services.UserServices;
@@ -49,4 +51,35 @@ public class UserController {
     public ResponseEntity<UserDTO> destroyUserById(@PathVariable UUID adminId, @PathVariable UUID organizationId, @RequestBody UserRequestUuid userRequest) {
         return ResponseEntity.ok(userServices.destroyUser(userRequest.userId()));
     }
+
+    @PutMapping("/updateusername/{userId}/{organizationId}")
+    @PreAuthorize("@helperUser.verifyUserAndOrganization(#userId, #organizationId, #userRequestBody.email)")
+    public ResponseEntity<UserDTO> updateUserName(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequestBody) {
+        return ResponseEntity.ok(userServices.updateUserName(userRequestBody.email(), userRequestBody.userName()));
+    }
+
+    @PutMapping("/updateispresident/{userId}/{organizationId}")
+    @PreAuthorize("@helperUser.isPresidentOrAdmin(#userId, #organizationId)")
+    public ResponseEntity<UserDTO> updateIsAdmin(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequest) {
+        return ResponseEntity.ok(userServices.updateIsAdmin(userRequest.userId(), userRequest.isAdmin()));
+    }
+
+    @PutMapping("/updateemail/{userId}/{organizationId}")
+    @PreAuthorize("@helperUser.verifyUserAndOrganization(#userId, #organizationId, #userRequestBody.email)")
+    public ResponseEntity<UserDTO> updatePassword(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequestBody) {
+        return ResponseEntity.ok(userServices.updatePassword(userRequestBody.email(), userRequestBody.Password()));
+    }
+
+    @PutMapping("updatepassword/{userId}/{organizationId}")
+    @PreAuthorize("@helperUser.verifyUserAndOrganization(#userId, #organizationId, #userRequestBody.email)")
+    public ResponseEntity<UserDTO> updateUserEmail(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequestBody) {
+        return ResponseEntity.ok(userServices.updateUserEmail(userRequestBody.email(), userRequestBody.newEmail()));
+    }
+
+    @PutMapping("/updateispresident/{userId}/{organizationId}")
+    @PreAuthorize("@helperUser.isPresidentOrAdmin(#userId, #organizationId)")
+    public ResponseEntity<UserDTO> updateUserOrganization(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequest) {
+        return ResponseEntity.ok(userServices.updateUserOrganization(userRequest.userId(), userRequest.organizationId()));
+    }   
+
 }
