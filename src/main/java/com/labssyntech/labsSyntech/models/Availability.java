2a29bad196c9f703ca21724143a63530881739bd
Wesley Silva <wesleycauan01@gmail.com>
2025-02-26 07:08:52 -0300

    Corrige relação entre sala e disponibilidade

--- src/main/java/com/labssyntech/labsSyntech/models/Availability.java
+++ src/main/java/com/labssyntech/labsSyntech/models/Availability.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,7 +26,7 @@ public class Availability {
     @JoinColumn(name = "fk_available_hours_id", referencedColumnName = "available_hours_id", nullable = false)
     private AvailableHours fkAvailableHours;
     
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "fk_room_id", referencedColumnName = "room_id", nullable = false)
     private Room fkRoom;
 
