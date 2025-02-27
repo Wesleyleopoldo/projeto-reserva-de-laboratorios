3d58078cb7ba3a5b48715023f3a8ed5658423c4a
Wesley Silva <wesleycauan01@gmail.com>
2025-02-27 07:35:44 -0300

    Adiciona comentários no controller

--- src/main/java/com/labssyntech/labsSyntech/controllers/UserController.java
+++ src/main/java/com/labssyntech/labsSyntech/controllers/UserController.java
@@ -29,53 +29,62 @@ public class UserController {
     @Autowired
     private UserServices userServices;
 
+    // Endpoint para cadastro de presidente
     @PostMapping("/signupPresident")
     public ResponseEntity<UserDTO> signupPresident(@RequestBody PresidentSignupRequest signupRequest) {
         return ResponseEntity.ok(userServices.signupPresidentService(signupRequest));
     }
 
+    // Endpoint para cadastro de usuário por um administrador ou presidente
     @PostMapping("/signupUser/{adminId}/{organizationId}")
     @PreAuthorize("@helperUser.isPresidentOrAdmin(#adminId, #organizationId)")
-    public ResponseEntity<UserDTO> signupUser(@RequestBody UserSignupRequest signupRequest, @PathVariable UUID adminId,@PathVariable UUID organizationId) {
+    public ResponseEntity<UserDTO> signupUser(@RequestBody UserSignupRequest signupRequest, @PathVariable UUID adminId, @PathVariable UUID organizationId) {
         return ResponseEntity.ok(userServices.signupUserService(signupRequest, organizationId));
     }
 
+    // Endpoint para obter todos os usuários, acessível apenas por administradores
     @GetMapping("/administerW/{userId}")
     @PreAuthorize("@helperUser.isAdminSyntech(#userId)")
     public ResponseEntity<List<UserDTO>> getAllUsers(@PathVariable UUID userId) {
         return ResponseEntity.ok(userServices.getAllUsersServices());
     }
 
+    // Endpoint para deletar um usuário por um administrador ou presidente
     @DeleteMapping("/destroyuser/{adminId}/{organizationId}")
     @PreAuthorize("@helperUser.isPresidentOrAdmin(#adminId, #organizationId)")
     public ResponseEntity<UserDTO> destroyUserById(@PathVariable UUID adminId, @PathVariable UUID organizationId, @RequestBody UserRequestUuid userRequest) {
         return ResponseEntity.ok(userServices.destroyUser(userRequest.userId()));
     }
 
+    // Endpoint para atualizar o nome de usuário
     @PutMapping("/updateusername/{userId}/{organizationId}")
     @PreAuthorize("@helperUser.verifyUserAndOrganization(#userId, #organizationId, #userRequestBody.email)")
     public ResponseEntity<UserDTO> updateUserName(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequestBody) {
         return ResponseEntity.ok(userServices.updateUserName(userRequestBody.email(), userRequestBody.userName()));
     }
 
+    // Endpoint para atualizar se o usuário é administrador
     @PutMapping("/updateispresident/{userId}/{organizationId}")
     @PreAuthorize("@helperUser.isPresidentOrAdmin(#userId, #organizationId)")
     public ResponseEntity<UserDTO> updateIsAdmin(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequest) {
         return ResponseEntity.ok(userServices.updateIsAdmin(userRequest.userId(), userRequest.isAdmin()));
     }
 
+    // Endpoint para atualizar a senha do usuário
     @PutMapping("/updateemail/{userId}/{organizationId}")
     @PreAuthorize("@helperUser.verifyUserAndOrganization(#userId, #organizationId, #userRequestBody.email)")
     public ResponseEntity<UserDTO> updatePassword(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequestBody) {
         return ResponseEntity.ok(userServices.updatePassword(userRequestBody.email(), userRequestBody.Password()));
     }
 
+    // Endpoint para atualizar o email do usuário
     @PutMapping("updatepassword/{userId}/{organizationId}")
     @PreAuthorize("@helperUser.verifyUserAndOrganization(#userId, #organizationId, #userRequestBody.email)")
     public ResponseEntity<UserDTO> updateUserEmail(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequestBody) {
         return ResponseEntity.ok(userServices.updateUserEmail(userRequestBody.email(), userRequestBody.newEmail()));
     }
 
+    // Endpoint para atualizar a organização do usuário
     @PutMapping("/updateispresident/{userId}/{organizationId}")
     @PreAuthorize("@helperUser.isPresidentOrAdmin(#userId, #organizationId)")
     public ResponseEntity<UserDTO> updateUserOrganization(@PathVariable UUID userId, @PathVariable UUID organizationId, @RequestBody UserRequestBody userRequest) {
