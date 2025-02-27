b1e5b305ccca2b856a90780156c5c2ac4b221daf
Wesley Silva <wesleycauan01@gmail.com>
2025-02-27 07:50:38 -0300

    Comentando o Organization Service

--- src/main/java/com/labssyntech/labsSyntech/services/OrganizationService.java
+++ src/main/java/com/labssyntech/labsSyntech/services/OrganizationService.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-
 @Service
 public class OrganizationService {
 
@@ -29,38 +28,45 @@ public class OrganizationService {
     @Autowired
     private HelperOrganization helperOrganization;
 
-    public List<OrganizationDTO> getAllOrganizationService()
-    {
+    // Método para obter todas as organizações
+    public List<OrganizationDTO> getAllOrganizationService() {
         List<Organization> listOrganizations = organizationRepository.findAll();
 
+        // Verifica se a lista de organizações está vazia
         if (listOrganizations.isEmpty()) {
             throw new NotFoundException("Não organizações a serem listadas");
         }
 
-        List<OrganizationDTO> listOrganizationDTO = listOrganizations.stream().map(organization -> new OrganizationDTO(organization.getOrganizationId(), organization.getOrganizationName())).toList();
+        // Converte a lista de Organization para OrganizationDTO
+        List<OrganizationDTO> listOrganizationDTO = listOrganizations.stream()
+            .map(organization -> new OrganizationDTO(organization.getOrganizationId(), organization.getOrganizationName()))
+            .toList();
         return listOrganizationDTO;
     }
 
+    // Método para obter uma organização pelo UUID
     public OrganizationDTO getOrganizationForUUID(UUID organizationId) {
         Organization organization = helperOrganization.findOrganizationById(organizationId, "Nenhuma organização encontrada...");
         OrganizationDTO organizationDTO = new OrganizationDTO(organization);
         return organizationDTO;
     }
 
+    // Método para criar uma nova organização
     public OrganizationDTO createOrganization(UUID userId, String name) {
         
+        // Verifica se a organização já existe pelo nome
         Optional<Organization> newOrganizationOptional = organizationRepository.findByOrganizationName(name);
 
-        if(newOrganizationOptional.isPresent())
-        {
+        if(newOrganizationOptional.isPresent()) {
             throw new ResourceAlredyExistsException("Essa empresa já foi cadastrada!!!");
         }
 
+        // Cria uma nova organização e salva no repositório
         Organization newOrganization = new Organization(name);
         organizationRepository.save(newOrganization);
 
+        // Define o usuário como presidente da nova organização
         helperUser.setOrganization(userId, newOrganization);
-
         helperUser.setIsPresident(userId, true);
 
         OrganizationDTO organizationDTO = new OrganizationDTO(newOrganization.getOrganizationId(), newOrganization.getOrganizationName());
@@ -68,6 +74,7 @@ public class OrganizationService {
         return organizationDTO;
     }
 
+    // Método para deletar uma organização pelo UUID
     public String destroyOrganizationService(UUID organizationId) {
         UUID organizationUuid = organizationId;
         Organization organization = organizationRepository
@@ -82,9 +89,11 @@ public class OrganizationService {
         }
     }
 
+    // Método para atualizar o nome de uma organização
     public OrganizationDTO updateOrganizationName(UUID organizationId, String newOrganizationName) {
         Organization organization = helperOrganization.findOrganizationById(organizationId, "Não foi encontrada nenhuma organização com esse nome...");
 
+        // Atualiza o nome da organização e salva no repositório
         organization.setOrganizationName(newOrganizationName);
         organizationRepository.save(organization);
 
