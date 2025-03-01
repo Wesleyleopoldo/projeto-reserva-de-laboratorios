722bb1cef9ceab1b42d8a0d1be324a14322e50f5
Wesley Silva <wesleycauan01@gmail.com>
2025-02-28 21:51:38 -0300

    Corrige lógica de verificação de usuário e acesso na classe HelperUser

--- src/main/java/com/labssyntech/labsSyntech/helper/HelperUser.java
+++ src/main/java/com/labssyntech/labsSyntech/helper/HelperUser.java
@@ -86,7 +86,7 @@ public class HelperUser {
         Optional<User> userOptional = userRepository.findById(userId);
         Optional<User> userByEmailOptional = userRepository.findByEmail(userEmail);
 
-        if(userOptional.isEmpty() && userByEmailOptional.isEmpty()) {
+        if(userOptional.isEmpty() || userByEmailOptional.isEmpty()) {
             throw new NotFoundException("Usuário não encontrado ou não existe...");
         }
 
@@ -96,7 +96,7 @@ public class HelperUser {
         User userByEmail = userByEmailOptional.get();
         if(!isAdmin && !user.isPresident() && !user.getUserId().equals(userByEmail.getUserId())){
             throw new AccessDeniedException("Você não tem privilégios de administrador para executar essa ação!!!");
-        } else if(!isAdmin && !user.getOrganization().getOrganizationId().equals(organizationId)) {
+        } else if(!isAdmin && !userByEmail.getOrganization().getOrganizationId().equals(organizationId)) {
             throw new AccessDeniedException("Permissão negada: você não pertence a essa empresa...");
         }
 
