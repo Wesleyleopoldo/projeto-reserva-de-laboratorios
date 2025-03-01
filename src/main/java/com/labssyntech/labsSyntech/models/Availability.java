26c94f00bbdbddf1151e6b757be4c10df3bc6d76
Wesley Silva <wesleycauan01@gmail.com>
2025-03-01 03:06:29 -0300

    Renomeia tabela de (availability) para (availabilities) na entidade Availability

--- src/main/java/com/labssyntech/labsSyntech/models/Availability.java
+++ src/main/java/com/labssyntech/labsSyntech/models/Availability.java
@@ -10,7 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "availability")
+@Table(name = "availabilities")
 public class Availability {
 
     @Id
